1a1e736ecf0a1eb834faf165435bd07f0625a2a4
Lucas Macchi <lucasmacchi25@gmail.com>
2025-04-11 15:59:25 -0300

    ssl

--- src/utils/clientReturner.ts
+++ src/utils/clientReturner.ts
@@ -15,7 +15,7 @@ export default function () {
         host: db_host,
         port: db_port,
         database: db_databse,
-        ssl: false//{rejectUnauthorized: false}
+        ssl: {rejectUnauthorized: false}
     })
     return client
 }
