39403ca118373f69100e22ad8cda4576bc4cc8e9
LucasMacchi <lucasmacchi25@gmail.com>
2026-01-29 15:14:52 -0300

    email compras, compras viajes restriction, change compra viaje

--- src/utils/emailCompra.ts
+++ src/utils/emailCompra.ts
@@ -5,9 +5,38 @@ const url = process.env.FRONT_END_URL ?? 'ERROR'
 export default function (insumos: IinsumoCompra[], comentario: string, descripcion: string, proveedor: string, fechaSol: string, area: string, lugar: string,compra_id:number): string {
     const compraId = compra_id
     const completeUrl = url+"compras/"+compraId
-    let message = `Proveedor sugerido: ${proveedor}\n\nArea: ${area}\n\Lugar: ${lugar}\n\nFecha a requerir: ${fechaSol}\n\nDescripcion proporcionada por el solicitante: \n${descripcion}\n\nProductos solicitados:\n`
-    insumos.map((i) => message += i.descripcion+" - "+i.cantidad+"\n")
-    message += "\nComentarios:\n"+comentario
-    message += '\n'+completeUrl+'\n'
-    return message
+    let insumosStr = ""
+    insumos.map((i) => insumosStr += `
+    <tr>
+        <th style="border: 1px solid">${i.descripcion}</th>
+        <th style="border: 1px solid">${i.cantidad}</th>
+    </tr>
+    `)
+    const html = `
+    <html>
+        <h3>Proveedor sugerido: ${proveedor}</h3>
+        <h3>Area: ${area}</h3>
+        <h3>Lugar: ${lugar}</h3>
+        <h3>Fecha a requerir: ${fechaSol}</h3>
+        <h3>Descipcion proporcionada por el solicitante: </h3>
+        <p>${descripcion}</p>
+        <h3>Productos Solicitados: </h3>
+        <table>
+            <tbody>
+                <tr>
+                    <th style="border: 1px solid">INSUMO/SERVICIO</th>
+                    <th style="border: 1px solid">CANTIDAD</th>
+                </tr>
+                <tr>
+                    ${insumosStr}
+                </tr>
+            </tbody>
+        </table>
+        <h3>Comentarios: </h3>
+        <p>${comentario}</p>
+        <a href="${completeUrl}">Link a la solicitud de compra</a>
+    </html>
+    `
+
+    return html
 }
